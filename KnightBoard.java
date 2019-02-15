b1bda888db4980469f8864a9991bfdf31b0a24da
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-02-15 10:17:54 -0500

    adding working add and remove knight methods

--- KnightBoard.java
+++ KnightBoard.java
@@ -21,9 +21,25 @@ public class KnightBoard {
     return result;
   }
 
+  private boolean addKnight(int row, int col, int num) {
+    if (board[row][col] == 0) {
+      board[row][col] = num;
+      return true;
+    }
+    return false;
+  }
+
+  private int removeKnight(int row, int col) {
+    int i = board[row][col];
+    board[row][col] = 0;
+    return i;
+  }
+
   public static void main(String[] args) {
     KnightBoard k = new KnightBoard(4,5);
     k.board[2][4] = 4;
+    k.addKnight(1,3,1);
+    k.removeKnight(2,4);
     System.out.println(k);
   }
 }
