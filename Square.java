050bc468b7754b068d9d74c92b628c4a9afbbda8
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-02-28 10:06:20 -0500

    tested sort on an arraylist, made comparable,all works.

--- Square.java
+++ Square.java
@@ -1,5 +1,8 @@
-public class Square {
-  private int row, col, val;
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+public class Square implements Comparable<Square>{
+  private int row, col, val; // val represents the moves possible for this square.
 
   //row that has been designated a value.
   public Square(int r, int c, int v) {
@@ -30,6 +33,36 @@ public class Square {
 
   public void setValue(int v) {
     val = v;
+  }
+
+  public int compareTo(Square other) {
+    if (other.getValue() == this.getValue()) return 0;
+    if (other.getValue() < this.getValue()) return 1;
+    if (other.getValue() > this.getValue()) return -1;
+    return 0;
+  }
+  public String toString() {
+    return ""+val;
+  }
+
+  public static void main(String[] args) {
+    Square a = new Square(1, 1, 1);
+    Square b = new Square(2,5,0);
+    Square c = new Square(2,5,7);
+    Square d = new Square(2,5,2);
+    Square e = new Square(2,5,4);
+    Square f = new Square(2,5,9);
+
+    ArrayList<Square> s = new ArrayList<Square>();
+    s.add(a);
+    s.add(b);
+    s.add(c);
+    s.add(d);
+    s.add(e);
+    s.add(f);
+    Collections.sort(s);
+    System.out.println(s);
+
   }
 
 }
